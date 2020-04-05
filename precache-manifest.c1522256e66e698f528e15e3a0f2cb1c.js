16774e1b52c9e447d95639c195cb98cbb2309f84
Danendz <danilapayne@gmail.com>
2020-04-05 22:58:35 +0500

    Updates

---
+++ precache-manifest.c1522256e66e698f528e15e3a0f2cb1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc39f7af1d8051451da8798787244619",
+    "url": "/lessonsite/index.html"
+  },
+  {
+    "revision": "dd19ad8a2babc5737b09",
+    "url": "/lessonsite/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "75d5ff6f3723ff17e2e6",
+    "url": "/lessonsite/static/css/main.9925801a.chunk.css"
+  },
+  {
+    "revision": "dd19ad8a2babc5737b09",
+    "url": "/lessonsite/static/js/2.843b3040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lessonsite/static/js/2.843b3040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d5ff6f3723ff17e2e6",
+    "url": "/lessonsite/static/js/main.9704a0ef.chunk.js"
+  },
+  {
+    "revision": "cc1c415f73890556bec3",
+    "url": "/lessonsite/static/js/runtime-main.e5f2a39a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/lessonsite/static/media/SEGA.12a3db8b.woff"
+  }
+]);
